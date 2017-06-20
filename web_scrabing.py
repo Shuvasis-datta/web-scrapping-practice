34ab1a7d133efffae5776ed9ef0ded30a6a27177
Shuvasis <shuvasisdatta@gmail.com>
2017-06-20 16:20:12 +0600

    initial commit

---
+++ web_scrabing.py
@@ -0,0 +1,29 @@
+# Web Scrapping using Python.
+# Tutorial Link - https://medium.freecodecamp.com/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
+
+
+# import libraries
+import urllib.request as url
+from bs4 import BeautifulSoup
+
+# specify the url
+# ICC Chamipons Trophy 2017 India Vs South Africa Match From Cricbuzz.com 
+quote_page = 'http://www.cricbuzz.com/live-cricket-scores/16710/ind-vs-rsa-11th-match-group-b-icc-champions-trophy-2017'
+
+# query the website and return the html to the variable ‘page’
+page = url.urlopen(quote_page)
+
+
+# parse the html using beautiful soap and store in variable `soup`
+soup = BeautifulSoup(page, 'html.parser')
+
+# get the Mini Score
+status_box = soup.find('div', attrs={'class': 'cb-min-bat-rw'}).find('span', attrs={'class': 'cb-font-20'})
+
+status = status_box.text.strip() # strip() is used to remove starting and trailing
+print(status)
+
+# get the Current Run Rate
+crr_box = soup.find('div', attrs={'class': 'cb-min-bat-rw'}).find('span', attrs={'class': 'cb-font-12'})
+crr = crr_box.text.strip()
+print (crr)
